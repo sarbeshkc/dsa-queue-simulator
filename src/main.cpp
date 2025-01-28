02ead7cf5ce2545a60de0f5129e8ffd5372fda90
sarbeshkc <weebmaniac2314@gmail.com>
2025-01-28 06:47:55 +0545

    Not going to be using rust, furstrating

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+// Example program:
+// Using SDL3 to create an application window
+
+#include <SDL3/SDL.h>
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "An SDL3 window",                  // window title
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(10000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+
+    // Clean up
+    SDL_Quit();
+    return 0;
+}
+
+
+
+// #include <iostream>
+// int main() {
+//     std::cout << "Hello, world!" << std::endl;
+//     system("pause");  // Press any key to continue...
+//     return 0;
+// }
+
